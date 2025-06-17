2d7a1b6e2043d03c2428f379a0b61bf8ea69bce9
Artus <artuslama@gmail.com>
2025-06-17 14:47:38 +0200

    📱 Fix Overflow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center h-screen w-screen relative px-8">
+    <div class="flex flex-col items-center h-screen w-screen relative px-8 overflow-x-hidden">
         <ColorThemeToggle />
         <slot />
     </div>
